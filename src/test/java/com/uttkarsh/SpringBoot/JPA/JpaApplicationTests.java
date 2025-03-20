dae579249cbe8918f10ed92ead2bacf791159a9a
uttkarsh08 <uttkarshchugh01@gmail.com>
2025-03-20 19:35:57 +0530

    Initial commit

---
+++ src/test/java/com/uttkarsh/SpringBoot/JPA/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uttkarsh.SpringBoot.JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
